9113695cdf668c8db442865fa18aaf784843d4fc
alexserban26 <57912415+alexserban26@users.noreply.github.com>
2023-02-13 17:06:57 +0200

    Add pagination.

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -11,8 +11,14 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
-  currentCategoryId: number=1;
+  currentCategoryId: number = 1;
   searchMode: boolean = false;
+  pageNumber: number = 1;
+  pageSize: number = 5;
+  totalElements: number = 0;
+  previousCategoryId: number = 1;
+
+  previousKeyword: string = "";
 
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
@@ -24,23 +30,28 @@ export class ProductListComponent implements OnInit {
   listProducts() {
     this.searchMode = this.route.snapshot.paramMap.has('keyword');
     if(this.searchMode){
-      this.handleSearchProducts()
+      this.handleSearchProducts();
     }
     else{
       this.handleListProducts();
     }
+  }
 
-
+  updatePageSize(pageSize: string){
+    this.pageSize = +pageSize;
+    this.pageNumber = 1;
+    this.listProducts();
   }
 
   handleSearchProducts() {
     const keyword: string = this.route.snapshot.paramMap.get('keyword')!;
 
-    this.productService.searchProducts(keyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    if(this.previousKeyword != keyword){
+      this.pageNumber = 1;
+    }
+    this.previousKeyword = keyword;
+
+    this.productService.searchProductsPaginate(this.pageNumber - 1, this.pageSize, keyword).subscribe(this.processResult());
 
   }
 
@@ -54,11 +65,22 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      (      data: Product[]) => {
-        this.products = data;
-      }
-    )
+    if(this.previousCategoryId != this.currentCategoryId){
+      this.pageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    this.productService.getProductListPaginate(this.pageNumber - 1, this.pageSize, this.currentCategoryId)
+          .subscribe(this.processResult());
   }
 
+  processResult(){
+    return (data: any) =>{
+      this.products = data._embedded.products;
+      this.pageNumber = data.page.number + 1;
+      this.pageSize = data.page.size;
+      this.totalElements = data.page.totalElements;
+    }
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -22,6 +22,12 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
+  searchProductsPaginate(page: number, pageSize: number, keyword: string): Observable<GetResponseProducts> {
+    const searchUrl = this.baseUrl + '/search/findByNameContaining?name=' + keyword +'&page=' + page + '&size=' + pageSize;;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
 
   getProduct(productId: number): Observable<Product> {
     const getProductUrl = this.baseUrl + '/' + productId;
@@ -34,6 +40,13 @@ export class ProductService {
     const searchUrl = this.baseUrl + '/search/findByCategoryId?id=' + categoryId;
 
     return this.getProducts(searchUrl);
+  }
+
+  getProductListPaginate(page: number, pageSize: number, categoryId: number): Observable<GetResponseProducts>{
+
+    const searchUrl = this.baseUrl + '/search/findByCategoryId?id=' + categoryId +'&page=' + page + '&size=' + pageSize;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
@@ -53,6 +66,12 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
